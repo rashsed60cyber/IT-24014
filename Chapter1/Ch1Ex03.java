113abe21a787dbecfc456185ee904de146839474
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:36:08 +0600

    Create Ch1Ex03.java

---
+++ Chapter1/Ch1Ex03.java
@@ -0,0 +1,9 @@
+public class Ch1Ex03 {
+    public static void main(String[] args) {
+        // "JAVA" pattern print korar jonno space er dik-e kheyal rakhte hobe
+        System.out.println("    J     A     V     V    A");
+        System.out.println("    J    A A     V   V    A A");
+        System.out.println("J   J   AAAAA     V V    AAAAA");
+        System.out.println(" J J   A     A     V    A     A");
+    }
+}
