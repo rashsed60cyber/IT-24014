1bd8843b3d6fb743c2381f4d28d372f8333a1083
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:23:37 +0600

    Create Ch1Ex01.java

---
+++ Chapter1/Ch1Ex01.java
@@ -0,0 +1,9 @@
+
+public class Ch1Ex01 {
+    public static void main(String[] args) {
+        // Console-e tin-ti alada line print kora
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
