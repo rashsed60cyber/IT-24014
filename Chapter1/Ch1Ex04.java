822e41604e2a1733c8d14b267650defb9296ec9e
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:37:45 +0600

    Create Ch1Ex04.java

---
+++ Chapter1/Ch1Ex04.java
@@ -0,0 +1,12 @@
+public class Ch1Ex04 {
+    public static void main(String[] args) {
+        // Table-er header print kora
+        System.out.println("a    a^2    a^3");
+        
+        // Data print kora (System.out.println diye protiti line alada kora hoyeche)
+        System.out.println("1    1      1");
+        System.out.println("2    4      8");
+        System.out.println("3    9      27");
+        System.out.println("4    16     64");
+    }
+}
