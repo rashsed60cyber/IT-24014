a5bf908550a2f34e21eaa1c5810bf9f1c32a912e
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:54:51 +0600

    Create Ch1Ex08.java

---
+++ Chapter1/Ch1Ex08.java
@@ -0,0 +1,17 @@
+public class Ch1Ex08 {
+    public static void main(String[] args) {
+        // Radius and Pi constants
+        double radius = 5.5;
+        double pi = 3.14159;
+
+        // Formula: perimeter = 2 * radius * pi
+        double perimeter = 2 * radius * pi;
+
+        // Formula: area = radius * radius * pi
+        double area = radius * radius * pi;
+
+        // Printing results
+        System.out.println("Perimeter = " + perimeter);
+        System.out.println("Area = " + area);
+    }
+}
