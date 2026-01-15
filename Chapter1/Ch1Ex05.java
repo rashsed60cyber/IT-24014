d413e99cd783e017012e5272da1e7d7a80b94e54
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:43:50 +0600

    Create Ch1Ex05.java

---
+++ Chapter1/Ch1Ex05.java
@@ -0,0 +1,8 @@
+public class Ch1Ex05 {
+    public static void main(String[] args) {
+        // Exercise 1.5 er math expression ti calculate kora hoyeche
+        // (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
+        System.out.println("Result of (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) is:");
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+}
