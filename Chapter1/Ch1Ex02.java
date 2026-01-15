46a7fcd9a55c4bc97725f4acd50fb0dd5a82cbb7
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:31:35 +0600

    Create Ch1Ex02.java

---
+++ Chapter1/Ch1Ex02.java
@@ -0,0 +1,10 @@
+public class Ch1Ex02 {
+    public static void main(String[] args) {
+        // "Welcome to Java" lekha-ti 5 bar print kora
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
