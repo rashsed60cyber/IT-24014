54826d4214b45f26992ff40f3d6b1bbe8b0035be
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 11:20:01 +0600

    Delete Ch1Ex06.java

--- Ch1Ex06.java
+++
@@ -1,6 +0,0 @@
-public class Ch1Ex06 {
-    public static void main(String[] args) {
-        // 1 theke 9 porjonto shonkha gulor jogfol
-        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
-    }
-}
