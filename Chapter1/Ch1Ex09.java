d796d730d88d5d0a4dc480d298848fea463e0447
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 10:55:38 +0600

    Create Ch1Ex09.java

---
+++ Chapter1/Ch1Ex09.java
@@ -0,0 +1,17 @@
+public class Ch1Ex09 {
+    public static void main(String[] args) {
+        // Sir-er deya example onujayi data
+        double width = 4.5;
+        double height = 7.9;
+
+        // Formula: area = width * height
+        double area = width * height;
+
+        // Formula: perimeter = 2 * (width + height)
+        double perimeter = 2 * (width + height);
+
+        // Printing results with labels
+        System.out.println("Area of the rectangle is: " + area);
+        System.out.println("Perimeter of the rectangle is: " + perimeter);
+    }
+}
